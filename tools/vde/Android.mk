64685bf0ec951bfc62f6508752b7f6dff982a490
harald.mue <harald.mue@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2010-04-20 05:39:21 +0000

    VDE.

---
+++ tools/vde/Android.mk
@@ -0,0 +1,121 @@
+# Copyright (C) 2008 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# vde_switch
+LOCAL_ARM_MODE := arm
+
+src_files :=  	src/common/open_memstream.c\
+		src/common/canonicalize.c\
+		src/vde_switch/consmgmt.c\
+		src/vde_switch/datasock.c\
+		src/vde_switch/fstp.c\
+		src/vde_switch/hash.c\
+		src/vde_switch/packetq.c\
+		src/vde_switch/port.c\
+		src/vde_switch/qtimer.c\
+		src/vde_switch/sockutils.c\
+		src/vde_switch/tuntap.c\
+		src/vde_switch/vde_switch.c
+
+LOCAL_SRC_FILES := $(src_files)
+LOCAL_C_INCLUDES:= external/vde/include\
+		external/vde/src/vde_switch\
+		external/vde/src/common
+
+LOCAL_LDFLAGS:= -lm -lc -lgcc 
+LOCAL_MODULE := vde_switch 
+LOCAL_CFLAGS += -O3 -DUSE_MMAP -Wall
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils
+
+include $(BUILD_EXECUTABLE)
+
+# slirpvde 
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+src_files :=    src/common/open_memstream.c\
+		src/common/canonicalize.c\
+		src/lib/libvdeplug.c \
+		src/slirpvde/bootp.c\
+		src/slirpvde/cksum.c\
+		src/slirpvde/debug.c\
+		src/slirpvde/if.c\
+		src/slirpvde/ip_icmp.c\
+		src/slirpvde/ip_input.c\
+		src/slirpvde/ip_output.c\
+		src/slirpvde/mbuf.c\
+		src/slirpvde/misc.c\
+		src/slirpvde/sbuf.c\
+		src/slirpvde/slirp.c\
+		src/slirpvde/slirpvde.c\
+		src/slirpvde/socket.c\
+		src/slirpvde/tcp2unix.c\
+		src/slirpvde/tcp_input.c\
+		src/slirpvde/tcp_output.c\
+		src/slirpvde/tcp_subr.c\
+		src/slirpvde/tcp_timer.c\
+		src/slirpvde/udp.c
+
+LOCAL_SRC_FILES := $(src_files)
+LOCAL_C_INCLUDES:= external/vde/include\
+                external/vde/src/slirpvde\
+		external/vde/src/lib\
+                external/vde/src/common
+
+LOCAL_LDFLAGS:= -lm -lc -lgcc
+LOCAL_MODULE := slirpvde 
+LOCAL_CFLAGS += -O3 -DUSE_MMAP -Wall
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils
+
+include $(BUILD_EXECUTABLE)
+
+# vde_pcapplug
+
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+src_files :=  	src/common/open_memstream.c\
+		src/common/canonicalize.c\
+		src/lib/libvdeplug.c \
+		src/vde_pcapplug.c
+
+LOCAL_SRC_FILES := $(src_files)
+LOCAL_C_INCLUDES:= external/vde/include\
+		external/vde/src\
+		external/vde/src/common\
+	        external/libpcap 
+
+
+LOCAL_LDFLAGS:= -lm -lc -lgcc 
+LOCAL_MODULE := vde_pcapplug 
+LOCAL_CFLAGS += -O3 -DUSE_MMAP -Wall
+
+LOCAL_STATIC_LIBRARIES+=libpcap
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libutils
+
+include $(BUILD_EXECUTABLE)
---
+++ tools/vde/include/config.h
@@ -0,0 +1,357 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
+/* include/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the `atexit' function. */
+#define HAVE_ATEXIT 1
+
+/* Define to 1 if you have the `bcmp' function. */
+#define HAVE_BCMP 1
+
+/* Define to 1 if your system has a working `chown' function. */
+#define HAVE_CHOWN 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the `drand48' function. */
+#define HAVE_DRAND48 1
+
+/* Define to 1 if you have the `dup2' function. */
+#define HAVE_DUP2 1
+
+/* Define to 1 if you have the `fchmod' function. */
+#define HAVE_FCHMOD 1
+
+/* Define to 1 if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Define to 1 if you have the `funopen' function. */
+/* #undef HAVE_FUNOPEN */
+
+/* Define to 1 if you have the `gethostbyname' function. */
+#define HAVE_GETHOSTBYNAME 1
+
+/* Define to 1 if you have the `gethostid' function. */
+#define HAVE_GETHOSTID 1
+
+/* Define to 1 if you have the `gethostname' function. */
+#define HAVE_GETHOSTNAME 1
+
+/* Define to 1 if you have the `getopt_long_only' function. */
+#define HAVE_GETOPT_LONG_ONLY 1
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the `index' function. */
+#define HAVE_INDEX 1
+
+/* Define to 1 if you have the `inet_aton' function. */
+#define HAVE_INET_ATON 1
+
+/* Define to 1 if you have the `inet_ntoa' function. */
+#define HAVE_INET_NTOA 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `dl' library (-ldl). */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#define HAVE_MALLOC 1
+
+/* Define to 1 if you have the `memmove' function. */
+#define HAVE_MEMMOVE 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <openssl/blowfish.h> header file. */
+#define HAVE_OPENSSL_BLOWFISH_H 1
+
+/* Define to 1 if you have the `open_memstream' function. */
+#define HAVE_OPEN_MEMSTREAM 1
+
+/* Define to 1 if your system has a working poll() function. */
+#define HAVE_POLL 1
+
+/* Define to 1 if you have the `putenv' function. */
+#define HAVE_PUTENV 1
+
+/* Define to 1 if you have the `random' function. */
+#define HAVE_RANDOM 1
+
+/* Define to 1 if you have the `readv' function. */
+#define HAVE_READV 1
+
+/* Define to 1 if your system has a GNU libc compatible `realloc' function,
+   and to 0 otherwise. */
+#define HAVE_REALLOC 1
+
+/* Define to 1 if you have the `revoke' function. */
+/* #undef HAVE_REVOKE */
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define to 1 if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* Define to 1 if you have the `sprintf' function. */
+#define HAVE_SPRINTF 1
+
+/* Define to 1 if you have the `srandom' function. */
+#define HAVE_SRANDOM 1
+
+/* Define to 1 if you have the <stddef.h> header file. */
+#define HAVE_STDDEF_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strndup' function. */
+#define HAVE_STRNDUP 1
+
+/* Define to 1 if you have the `strstr' function. */
+#define HAVE_STRSTR 1
+
+/* Define to 1 if you have the <syslimits.h> header file. */
+/* #undef HAVE_SYSLIMITS_H */
+
+/* Define to 1 if you have the <syslog.h> header file. */
+#define HAVE_SYSLOG_H 1
+
+/* Define to 1 if you have the <sys/bitypes.h> header file. */
+//#define HAVE_SYS_BITYPES_H 0
+#undef HAVE_SYS_BITYPES_H
+
+/* Define to 1 if you have the <sys/filio.h> header file. */
+/* #undef HAVE_SYS_FILIO_H */
+
+/* Define to 1 if you have the <sys/ioctl.h> header file. */
+#define HAVE_SYS_IOCTL_H 1
+
+/* Define to 1 if you have the <sys/param.h> header file. */
+#define HAVE_SYS_PARAM_H 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/* Define to 1 if you have the <sys/signal.h> header file. */
+//#define HAVE_SYS_SIGNAL_H 1
+#undef HAVE_SYS_SIGNAL_H
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/stropts.h> header file. */
+//#define HAVE_SYS_STROPTS_H 1
+#undef HAVE_SYS_STROPTS_H
+
+/* Define to 1 if you have the <sys/syslimits.h> header file. */
+/* #undef HAVE_SYS_SYSLIMITS_H */
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/type32.h> header file. */
+/* #undef HAVE_SYS_TYPE32_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <sys/wait.h> header file. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the <termios.h> header file. */
+#define HAVE_TERMIOS_H 1
+
+/* Define to 1 if you have the <termio.h> header file. */
+#define HAVE_TERMIO_H 1
+
+/* If defined, tuntap support is compiled in */
+#define HAVE_TUNTAP 1
+
+/* Define to 1 if you have the `uname' function. */
+#define HAVE_UNAME 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vfork' function. */
+#define HAVE_VFORK 1
+
+/* Define to 1 if you have the <vfork.h> header file. */
+/* #undef HAVE_VFORK_H */
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Define to 1 if `fork' works. */
+#define HAVE_WORKING_FORK 1
+
+/* Define to 1 if `vfork' works. */
+#define HAVE_WORKING_VFORK 1
+
+/* PREFIX */
+#define INSTALLPATH "/usr/local"
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "vde2"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "info@v2.cs.unibo.it"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "vde2"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "vde2 2.2.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "vde2"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.2.3"
+
+/* Define to 1 if the C compiler supports function prototypes. */
+#define PROTOTYPES 1
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* Define to the type of arg 1 for `select'. */
+#define SELECT_TYPE_ARG1 int
+
+/* Define to the type of args 2, 3 and 4 for `select'. */
+#define SELECT_TYPE_ARG234 (fd_set *)
+
+/* Define to the type of arg 5 for `select'. */
+#define SELECT_TYPE_ARG5 (struct timeval *)
+
+/* The size of `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of `char *', as computed by sizeof. */
+#define SIZEOF_CHAR_P 4
+
+/* The size of `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* If defined, enable support for IPN socket */
+/* #undef USE_IPN */
+
+/* If defined, this is a Darwin system */
+/* #undef VDE_DARWIN */
+
+/* If defined, this is a FreeBSD system */
+/* #undef VDE_FREEBSD */
+
+/* If defined, this is a Linux system */
+#define VDE_LINUX 1
+
+/* Version number of package */
+#define VERSION "2.2.3"
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+/* #undef WORDS_BIGENDIAN */
+
+/* Define like PROTOTYPES; this can be used by system headers. */
+#define __PROTOTYPES 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef gid_t */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef mode_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to vde_poll if the replacement function should be used. */
+/* #undef poll */
+
+/* Define to rpl_realloc if the replacement function should be used. */
+/* #undef realloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> doesn't define. */
+/* #undef uid_t */
+
+/* Define as `fork' if `vfork' does not work. */
+/* #undef vfork */
