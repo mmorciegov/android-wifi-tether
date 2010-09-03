75c0c2ef469f5161a9b472b15999255dcc7190a9
harald.mue <harald.mue@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2010-09-03 14:25:35 +0000

    Check for directories - /proc/calibration or /data/calibration - see issue 525.

--- trunk/src/android/tether/TetherApplication.java
+++ trunk/src/android/tether/TetherApplication.java
@@ -286,6 +286,8 @@ public class TetherApplication extends Application {
 		else {
 			this.tethercfg.put("tether.fix.route", "false");
 		}
+		// adding proc-calibration-location
+		this.tethercfg.put("wifi.tiwlan.calibration", Configuration.getTiwlanCalibrationPath());
 		
 		/**
 		 * TODO: Quick and ugly workaround for nexus
