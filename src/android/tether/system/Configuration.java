7cb71663fe1587e5fb2d865fbd8671dce9d0290c
harald.mue <harald.mue@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2010-09-03 14:25:35 +0000

    Check for directories - /proc/calibration or /data/calibration - see issue 525.

--- src/android/tether/system/Configuration.java
+++ src/android/tether/system/Configuration.java
@@ -143,4 +143,14 @@ public class Configuration {
     	}
     	return false;
     }
+    
+    public static String getTiwlanCalibrationPath() {
+    	if ((new File("/proc/calibration")).exists()) {
+    		return "/proc/calibration";
+    	}
+    	else if ((new File("/data/calibration")).exists()) {
+    		return "/data/calibration";
+    	}
+    	return "none";
+    }
 }
