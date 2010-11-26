d58e4eed8a948e3a25dacf91b745feac2c356e91
harald.mue <harald.mue@gmail.com>
2010-11-26 12:30:42 +0000

    libnativetask is now compiled with NDK.

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,15 +1,8 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
-LOCAL_PRELINK_MODULE:=false
-
+LOCAL_MODULE    := libnativetask
 LOCAL_SRC_FILES := android_tether_system_NativeTask.c 
-
-LOCAL_SHARED_LIBRARIES := libcutils
-
-LOCAL_C_INCLUDES += $(JNI_H_INCLUDE)
-
-LOCAL_MODULE := libnativetask
+#LOCAL_SHARED_LIBRARIES := libcutils
 
 include $(BUILD_SHARED_LIBRARY)
