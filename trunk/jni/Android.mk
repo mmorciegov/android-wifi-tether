06eab2984899feb7c57afb596895827cfed94342
harald.mue@gmail.com <harald.mue@gmail.com@58ca5e2c-0207-11de-85c2-bb0660c75f22>
2012-07-10 19:36:24 +0000

    Cleanup trunk.

--- trunk/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := libnativetask
-LOCAL_SRC_FILES := android_tether_system_NativeTask.c 
-#LOCAL_SHARED_LIBRARIES := libcutils
-
-include $(BUILD_SHARED_LIBRARY)
